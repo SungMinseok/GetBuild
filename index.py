0b2d3227ef646d1a6f70bee11b6a0a28593be7c5
SungMinseok <70127676+SungMinseok@users.noreply.github.com>
2024-07-16 02:03:58 +0900

    feat: add timer UI

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ import os
 import shutil
 import json
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, 
-                             QLineEdit, QPushButton, QComboBox, QFileDialog, QMessageBox, QProgressDialog)
-from PyQt5.QtCore import Qt  # Qt 모듈을 임포트
+                             QLineEdit, QPushButton, QComboBox, QFileDialog, QMessageBox, QProgressDialog, QTimeEdit, QCheckBox)
+from PyQt5.QtCore import Qt, QTime, QTimer
 
 class FolderCopyApp(QWidget):
     def __init__(self):
@@ -20,6 +20,7 @@ class FolderCopyApp(QWidget):
         h_layout2 = QHBoxLayout()
         h_layout2_1 = QHBoxLayout()
         h_layout3 = QHBoxLayout()
+        h_layout4 = QHBoxLayout()
 
         # Create widgets for the first row
         self.input_box1 = QLineEdit(self)
@@ -60,7 +61,7 @@ class FolderCopyApp(QWidget):
         self.copy_button.clicked.connect(lambda : self.copy_folder(self.input_box2.text(),'WindowsClient'))
         self.copy_button1 = QPushButton('Copy Server', self)
         self.copy_button1.clicked.connect(lambda : self.copy_folder(self.input_box2_1.text(),'WindowsServer'))
-        self.copy_button2 = QPushButton('Copy Both', self)
+        self.copy_button2 = QPushButton('Copy All', self)
         #self.copy_button1.clicked.connect(self.copy_folder)
 
         # Add widgets to the third row layout
@@ -70,19 +71,40 @@ class FolderCopyApp(QWidget):
         h_layout3.addWidget(self.copy_button1)
         h_layout3.addWidget(self.copy_button2)
 
+        # Create widgets for the time setting
+        self.time_edit = QTimeEdit(self)
+        self.time_edit.setDisplayFormat("HH:mm")
+        self.input_box4 = QLineEdit(self)
+        self.checkbox = QCheckBox('Reservation', self)
+
+
+
+        # Add widgets to the main layout
+        h_layout4.addWidget(self.time_edit)
+        h_layout4.addWidget(self.input_box4)
+        h_layout4.addWidget(self.checkbox)
+
         # Add row layouts to the main layout
         layout.addLayout(h_layout1)
         layout.addLayout(h_layout2)
         layout.addLayout(h_layout2_1)
         layout.addLayout(h_layout3)
+        layout.addLayout(h_layout4)
 
         # Set the main layout
         self.setLayout(layout)
+
 
         # Set the window properties
         self.setWindowTitle('Get Build')
         self.setGeometry(300, 300, 650, 100)
         self.show()
+
+
+        # Set up a timer to check every minute
+        self.check_timer = QTimer(self)
+        self.check_timer.timeout.connect(self.check_time)
+        self.check_timer.start(1000)  # 60,000 milliseconds = 1 minute
 
     def choose_folder(self, return_input_box):
         folder_path = QFileDialog.getExistingDirectory(self, 'Select Folder')
@@ -103,7 +125,7 @@ class FolderCopyApp(QWidget):
             folders.sort(key=lambda x: os.path.getmtime(os.path.join(folder_path, x)), reverse=True)
             self.combo_box.addItems(folders)
 
-    def copy_folder(self, dest_folder, target_name):
+    def copy_folder(self, dest_folder, target_folder, target_name):
         '''
         dest_folder = 'C:/mybuild'
         target_name = 'WindowsClient'
@@ -158,6 +180,17 @@ class FolderCopyApp(QWidget):
         except:
             QMessageBox.critical(self, 'Error', 'Invalid directory.')
 
+    def check_time(self):
+        if self.checkbox.isChecked():
+            current_time = QTime.currentTime()
+            set_time = self.time_edit.time()
+            if current_time.hour() == set_time.hour() and current_time.minute() == set_time.minute():
+                self.test_timer()
+                self.checkbox.setChecked(False)
+
+    def test_timer(self):
+        QMessageBox.information(self, 'Test', 'Timer executed.')
+
     def load_settings(self):
         if os.path.exists(self.settings_file):
             with open(self.settings_file, 'r') as file:
@@ -166,13 +199,19 @@ class FolderCopyApp(QWidget):
                 self.input_box2.setText(settings.get('input_box2', ''))
                 self.input_box2_1.setText(settings.get('input_box2_1', ''))
                 self.combo_box.addItems(settings.get('combo_box', []))
+                self.input_box4.setText(settings.get('input_box4', ''))
+                time_value = settings.get('time_edit', '')
+            if time_value:
+                self.time_edit.setTime(QTime.fromString(time_value, 'HH:mm'))
 
     def save_settings(self):
         settings = {
             'input_box1': self.input_box1.text(),
             'input_box2': self.input_box2.text(),
             'input_box2_1': self.input_box2_1.text(),
-            'combo_box': [self.combo_box.itemText(i) for i in range(self.combo_box.count())]
+            'combo_box': [self.combo_box.itemText(i) for i in range(self.combo_box.count())],
+            'input_box4': self.input_box4.text(),
+            'time_edit': self.time_edit.time().toString('HH:mm'),
         }
         with open(self.settings_file, 'w') as file:
             json.dump(settings, file)
