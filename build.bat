4df07c972a6a809088fbdfb36f1e300658291fe7
SungMinseok <gaegumun@gmail.com>
2024-09-20 13:34:38 +0900

    빌드bat파일 수정 : exe 파일명 QuickBuild로 변경

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ REM Activate the virtual environment
 call C:\myvenv\getbuild\Scripts\activate.bat
 
 REM Run PyInstaller with the specified options
-pyinstaller --upx-dir C:\upx-4.2.4-win64 -F -w -i pbb.ico index.py
+pyinstaller --upx-dir C:\upx-4.2.4-win64 -F -w -i ico.ico --name QuickBuild index.py
 
 REM Pause to see the output if running manually
 pause
